7c19e20d33650af4de9f792cd51b1179d0c2a0f6
haishangqishi <ahaishangqishi@163.com>
2016-09-14 08:59:06 +0800

    王睿：1）添加ContractController，和service及实现，dao及实现；2）同时修改dao层中王慧敏的方法；3）修改user实体中user_id字段。

--- src/com/mvc/dao/ContractDao.java
+++ src/com/mvc/dao/ContractDao.java
@@ -1,13 +1,16 @@
 package com.mvc.dao;
 
-/**
- * 合同
- * @author wanghuimin
- * @date 2016年9月9日
- */
-public interface ContractDao {
-	
-	// 根据合同id修改状态
-	public boolean updateState(Integer id, Integer state);
+import java.util.List;
 
+import com.mvc.entity.Contract;
+
+public interface ContractDao {
+
+	public boolean updateState(Integer cont_id, Integer cont_state);
+	
+	public List<Contract> findAllCont(int creator_id);
+	
+	public List<Contract> findAllDebtCont(int creator_id);
+	
+	public List<Contract> findAllOverdueCont(int creator_id);
 }
--- src/com/mvc/dao/impl/ContractDaoImpl.java
+++ src/com/mvc/dao/impl/ContractDaoImpl.java
@@ -1,5 +1,7 @@
 package com.mvc.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
@@ -9,12 +11,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import com.mvc.dao.ContractDao;
+import com.mvc.entity.Contract;
 
 /**
  * 合同
  * 
- * @author wanghuimin
- * @date 2016年9月9日
+ * @author wangrui
+ * @date 2016年9月13日
  */
 @Repository("contractDaoImpl")
 public class ContractDaoImpl implements ContractDao {
@@ -23,13 +26,13 @@ public class ContractDaoImpl implements ContractDao {
 	EntityManagerFactory emf;
 
 	// 根据合同id修改状态
-	public boolean updateState(Integer id, Integer state) {
+	public boolean updateState(Integer cont_id, Integer cont_state) {
 		EntityManager em = emf.createEntityManager();
 		try {
-			String selectSql = " update contract set 'cont_state' = :cont_state  where cont_id =:cont_id ";
+			String selectSql = "update contract c set c.cont_state=:state where c.cont_id=:cont_id";
 			Query query = em.createNativeQuery(selectSql);
-			query.setParameter("cont_state", state);
-			query.setParameter("cont_id", id);
+			query.setParameter("cont_state", cont_state);
+			query.setParameter("cont_id", cont_id);
 			query.executeUpdate();
 			em.flush();
 			em.getTransaction().commit();
@@ -37,6 +40,45 @@ public class ContractDaoImpl implements ContractDao {
 			em.close();
 		}
 		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Contract> findAllCont(int creator_id) {
+		EntityManager em = emf.createEntityManager();
+		String sql = "select * from contract c where c.user_id=:creator_id and c.cont_ishistory=0";
+		// 创建原生SQL查询QUERY实例,指定了返回的实体类型
+		Query query = em.createNativeQuery(sql, Contract.class);
+		query.setParameter("creator_id", creator_id);
+		List<Contract> list = query.getResultList();
+		em.close();
+		return list;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Contract> findAllDebtCont(int creator_id) {
+		EntityManager em = emf.createEntityManager();
+		String sql = "select * from contract c where c.cont_id in (select rn.cont_id from receive_node rn "
+				+ "where rn.reno_time<=now() and rn.reno_state in (0,2)) and c.user_id=:creator_id and c.cont_ishistory=0";
+		Query query = em.createNativeQuery(sql, Contract.class);
+		query.setParameter("creator_id", creator_id);
+		List<Contract> list = query.getResultList();
+		em.close();
+		return list;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Contract> findAllOverdueCont(int creator_id) {
+		EntityManager em = emf.createEntityManager();
+		String sql = "select * from contract c where c.cont_id in (select t.cont_id from task t "
+				+ "where t.task_etime<=now() and t.task_state in (0,1) and t.task_isdelete=0) and c.user_id=:creator_id and c.cont_ishistory=0";
+		Query query = em.createNativeQuery(sql, Contract.class);
+		query.setParameter("creator_id", creator_id);
+		List<Contract> list = query.getResultList();
+		em.close();
+		return list;
 	}
 
 }
