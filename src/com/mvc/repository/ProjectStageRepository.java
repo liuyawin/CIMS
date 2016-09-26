a19873e88bc2a8b6bae3a0840825d803f11bc1a2
haishangqishi <ahaishangqishi@163.com>
2016-09-26 15:59:15 +0800

    .

---
+++ src/com/mvc/repository/ProjectStageRepository.java
@@ -0,0 +1,23 @@
+package com.mvc.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.mvc.entity.ProjectStage;
+
+/**
+ * 工期阶段JPA
+ * 
+ * @author wangrui
+ * @date 2016-09-20
+ */
+public interface ProjectStageRepository extends JpaRepository<ProjectStage, Integer> {
+
+	// 查询该合同的工期阶段
+	@Query("select ps from ProjectStage ps where cont_id=:cont_id")
+	List<ProjectStage> selectPrstByContId(@Param("cont_id") Integer cont_id);
+
+}
