0bc84f0d83fbf3ca401b726066af8b8fcc14e510
haishangqishi <ahaishangqishi@163.com>
2016-11-07 17:01:56 +0800

    1）修改LoginController； 2）修改合同业务层； 3）添加ContractState枚举类；

--- src/com/mvc/controller/LoginController.java
+++ src/com/mvc/controller/LoginController.java
@@ -123,6 +123,7 @@ public class LoginController {
 	 * @param res
 	 * @return
 	 */
+	@SuppressWarnings("unused")
 	@RequestMapping("/login.do")
 	public String login(HttpSession session, HttpServletRequest request, ModelMap model, HttpServletResponse res) {
 		String error_msg = "";
@@ -130,15 +131,12 @@ public class LoginController {
 		String password = request.getParameter("password");
 		String isRemember = request.getParameter("isRemember"); // 记住密码//值获取不到
 		User user = userService.findByUserNum(userNum);
-		System.out.println("权限测试开始：");
 		String result = "";
 		String permission = "";
 		if (user.getRole().getRole_permission() != null && !user.getRole().getRole_permission().equals("")) {
 			permission = user.getRole().getRole_permission();
 			result = numToPermissionStr(permission);
 		}
-		System.out.println("权限result：" + result);
-		System.out.println("权限测试结束");
 
 		CookieUtil cookie_u = new CookieUtil();
 		if (user != null) { // 用户存在
@@ -213,18 +211,59 @@ public class LoginController {
 		User user = (User) session.getAttribute(SessionKeyConstants.LOGIN);
 		if (user != null) {
 			AlarmStatistic alarmStatistic = alarmStatisticService.findAlst(user.getUser_id());
-			jsonObject.put("totalReceiveTaskNum", alarmStatistic.getTotal_receive_task_num());
-			jsonObject.put("waitAuditBillTaskNum", alarmStatistic.getWait_audit_bill_task_num());
-			jsonObject.put("assistantTaskNum", alarmStatistic.getAssistant_task_num());
-			jsonObject.put("managerControlTaskNum", alarmStatistic.getManager_control_task_num());
-			jsonObject.put("billTaskNum", alarmStatistic.getBill_task_num());
-			jsonObject.put("otherTaskNum", alarmStatistic.getOther_task_num());
-			jsonObject.put("debtAlarmNum", alarmStatistic.getDebt_alarm_num());
-			jsonObject.put("overdueAlarmNum", alarmStatistic.getOverdue_alarm_num());
-			jsonObject.put("taskAlarmNum", alarmStatistic.getTask_alarm_num());
-			jsonObject.put("remoTaskNum", alarmStatistic.getRemo_task_num());
+			jsonObject.put("waitAuditBillTaskNum", alarmStatistic.getWait_audit_bill_task_num());// 待审核发票任务
+			jsonObject.put("assistantTaskNum", alarmStatistic.getAssistant_task_num());// 文书任务
+			jsonObject.put("managerControlTaskNum", alarmStatistic.getManager_control_task_num());// 执行管控任务
+			jsonObject.put("billTaskNum", alarmStatistic.getBill_task_num());// 发票任务
+			jsonObject.put("otherTaskNum", alarmStatistic.getOther_task_num());// 普通任务
+			jsonObject.put("debtAlarmNum", alarmStatistic.getDebt_alarm_num());// 收款超时
+			jsonObject.put("overdueAlarmNum", alarmStatistic.getOverdue_alarm_num());// 工程逾期
+			jsonObject.put("taskAlarmNum", alarmStatistic.getTask_alarm_num());// 任务超时
+			jsonObject.put("remoTaskNum", alarmStatistic.getRemo_task_num());// 待核对到款任务
+			jsonObject.put("totalReceiveTaskNum", calTotalNum(alarmStatistic, user));// 当前用户接收的所有任务
 		}
 		return jsonObject.toString();
+	}
+
+	/**
+	 * 根据权限计算总任务数
+	 * 
+	 * @param alarmStatistic
+	 * @param user
+	 * @return
+	 */
+	private Integer calTotalNum(AlarmStatistic alarmStatistic, User user) {
+		Integer wait_audit_bill_task_num = alarmStatistic.getWait_audit_bill_task_num();// 待审核发票任务
+		Integer assistant_task_num = alarmStatistic.getAssistant_task_num();// 文书任务
+		Integer manager_control_task_num = alarmStatistic.getManager_control_task_num();// 执行管控任务
+		Integer bill_task_num = alarmStatistic.getBill_task_num();// 发票任务
+		Integer other_task_num = alarmStatistic.getOther_task_num();// 普通任务
+		Integer remo_task_num = alarmStatistic.getRemo_task_num();// 待核对到款任务
+
+		String result = "";
+		String permission = user.getRole().getRole_permission();
+		if (permission != null && !permission.equals("")) {
+			result = numToPermissionStr(permission);
+		}
+
+		Integer total_num = other_task_num;// 每个人都有普通任务
+		if (result.contains("iAssiTask")) {
+			total_num += assistant_task_num;
+		}
+		if (result.contains("iEditTask")) {
+			total_num += manager_control_task_num;
+		}
+		if (result.contains("iAudiInvoTask")) {
+			total_num += wait_audit_bill_task_num;
+		}
+		if (result.contains("iFiniInvoTask")) {
+			total_num += bill_task_num;
+		}
+		if (result.contains("iFiniRemoTask")) {
+			total_num += remo_task_num;
+		}
+
+		return total_num;
 	}
 
 	/**
@@ -265,29 +304,31 @@ public class LoginController {
 	private static String toPermissionStr(String str, String type, String result) {
 		String subStr = str.substring(1, str.length() - 1);
 		String strArr[] = subStr.split(",");
+		StringBuilder strb = new StringBuilder();
+		strb.append(result);
 		for (int i = 0; i < strArr.length; i++) {
 			if (strArr[i].equals("1")) {
 				switch (type) {
 				case "contPer":
-					result += " " + PermissionConstants.contPer[i];
+					strb.append(" " + PermissionConstants.contPer[i]);
 					break;
 				case "taskPer":
-					result += " " + PermissionConstants.taskPer[i];
+					strb.append(" " + PermissionConstants.taskPer[i]);
 					break;
 				case "billPer":
-					result += " " + PermissionConstants.billPer[i];
+					strb.append(" " + PermissionConstants.billPer[i]);
 					break;
 				case "systemPer":
-					result += " " + PermissionConstants.systemPer[i];
+					strb.append(" " + PermissionConstants.systemPer[i]);
 					break;
 				case "indexPer":
-					result += " " + PermissionConstants.indexPer[i];
+					strb.append(" " + PermissionConstants.indexPer[i]);
 					break;
 				default:
 					break;
 				}
 			}
 		}
-		return result;
+		return strb.toString();
 	}
 }
